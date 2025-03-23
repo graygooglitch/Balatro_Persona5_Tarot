46373b6837e983b91c4075b26f9e8daca28f63f1
GrayGooGlitch <maxwell@brokengif.com>
2025-03-22 20:51:44 -0400

    Initial Commit

---
+++ main.lua
@@ -0,0 +1,47 @@
+AltTexture({
+    key = 'tarot_tex',
+    set = 'Tarot',
+    path = 'Tarots.png',
+    keys = {
+        'c_fool',
+        'c_magician',
+        'c_high_priestess',
+        'c_empress',
+        'c_emperor',
+        'c_heirophant',
+        'c_lovers',
+        'c_chariot',
+        'c_justice',
+        'c_hermit',
+        'c_wheel_of_fortune',
+        'c_strength',
+        'c_hanged_man',
+        'c_death',
+        'c_temperance',
+        'c_devil',
+        'c_tower',
+        'c_star',
+        'c_moon',
+        'c_sun',
+        'c_judgement',
+        'c_world'
+    },
+    original_sheet = 'true',
+    loc_txt = {
+      name = 'P5 Tarot'
+    }
+})
+
+TexturePack({
+    key = 'p5t_tarot_pack',
+    textures = {
+      'p5t_tarot_tex'
+    },
+    loc_txt = {
+      name = 'Persona 5 Tarot',
+      text = {'SMT Persona 5 Tarot Cards'}
+    }
+})
+
+----------------------------------------------
+------------MOD CODE END----------------------
